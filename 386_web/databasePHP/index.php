f7aa1facdd57aaeb55f3bcf43ce3338529b23438
vlexum <scottyaustin5@gmail.com>
2021-10-22 13:36:50 -0700

    PHP for Database Interactions
    connect-sql connects to the database and can be use whenever we need to connect to the database by calling "include ('connect-sql.php')";. display-data connects to the DB then fetches all info from the vehicle table.

---
+++ 386_web/databasePHP/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DB TEST</title>
+    </head>
+    <body>
+        <p>
+            <form action="display-data.php">
+                <input type="submit" value="click on me!">
+              </form>
+        </p>
+    </body>
+</html>
