157a8e6f79aff8ff5ddcb34241964979d4745841
Austin Malmin <89808565+whatup9911@users.noreply.github.com>
2021-10-23 10:10:48 -0700

    Update display-data.php

--- 386_web/databasePHP/display-data.php
+++ 386_web/databasePHP/display-data.php
@@ -1,27 +1,37 @@
 <?php
 
-include ('connect-sql.php');
+$Make = filter_input(INPUT_POST, 'Make')
+$Model = filter_input(INPUT_POST, 'Model')
+$Year = filter_input(INPUT_POST, 'Year')
+$Mileage = filter_input(INPUT_POST, 'Mileage')
+$Color = filter_input(INPUT_POST, 'Color')
 
-$sqlget = "SELECT * FROM ondemandcar.vehicle;";
-$sqldata = mysqli_query($dbcon, $sqlget) or die(mysqli_error($dbcon));
+if(!empty($Make) && !empty($Model) && !empty($Year) && !empty($Mileage)
+ && !empty($Color){
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "carinfo"
+   
+   // Create connection
+   $dbcon = mysqli_connect($servername, $username, $password, $dbname);
+ }
+ else{
+   echo "Please fill in all the entries"
+ }
 
-echo "<table>";
-echo "<tr><th>Make</th><th>Model</th><th>Year</th><th>Mileage</th><th>Color</th></tr>";
-
-while ($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)){
-    echo "<tr><td>";
-    echo $row['make'];
-    echo "</td><td>";
-    echo $row['model'];
-    echo "</td><td>";
-    echo $row['carYear'];
-    echo "</td><td>";
-    echo $row['mileage'];
-    echo "</td><td>";
-    echo $row['color'];
-    echo "</td></tr>";
+// Check connection
+if (!$dbcon) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-
-echo "</table>";
-
+echo "Connected successfully";
+$sql = "INSERT INTO carinfo( Make, Model, Year, Mileage, Color)
+values = ('$Make', '$Model','$Year','$Mileage','$Color')";
+if( $dbcon -> query$sqL){
+  echo "New vehicle is inserted successfully";
+}  
+else{
+  echo "Error: ". $sqL ."<br>". $dbcon->error  ;
+}
+$dbcon->close();
 ?>
