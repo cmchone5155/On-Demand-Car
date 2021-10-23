e1caeae06dedacaddf94bb63722fd7b0b41dad85
Austin Malmin <89808565+whatup9911@users.noreply.github.com>
2021-10-23 10:39:53 -0700

    Update connect-sql.php
    was missing a couple semicolons

--- 386_web/databasePHP/connect-sql.php
+++ 386_web/databasePHP/connect-sql.php
@@ -1,14 +1,37 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
 
-// Create connection
-$dbcon = mysqli_connect($servername, $username, $password);
+$Make = filter_input(INPUT_POST, 'Make');
+$Model = filter_input(INPUT_POST, 'Model');
+$Year = filter_input(INPUT_POST, 'Year');
+$Mileage = filter_input(INPUT_POST, 'Mileage');
+$Color = filter_input(INPUT_POST, 'Color');
+
+if(!empty($Make) && !empty($Model) && !empty($Year) && !empty($Mileage)
+ && !empty($Color){
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "carinfo";
+
+   // Create connection
+   $dbcon = mysqli_connect($servername, $username, $password, $dbname);
+ }
+ else{
+   echo "Please fill in all the entries"
+ }
 
 // Check connection
 if (!$dbcon) {
   die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+$sql = "INSERT INTO carinfo( Make, Model, Year, Mileage, Color)
+values = ('$Make', '$Model','$Year','$Mileage','$Color')";
+if( $dbcon -> query$sqL){
+  echo "New vehicle is inserted successfully";
+}
+else{
+  echo "Error: ". $sqL ."<br>". $dbcon->error  ;
+}
+$dbcon->close();
 ?>
